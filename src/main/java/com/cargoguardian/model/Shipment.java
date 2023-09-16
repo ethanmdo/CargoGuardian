5fde888ad936f6b75a9d64189ef3a2ad58fc7db7
Aneesh Sunkarapalli <56797251+AneeshSunkarapalli@users.noreply.github.com>
2023-09-15 23:42:00 -0400

    content

--- src/main/java/com/cargoguardian/model/Shipment.java
+++ src/main/java/com/cargoguardian/model/Shipment.java
@@ -2,6 +2,16 @@ package com.cargoguardian.model;
 
 public class Shipment {
 
-    private content;
+    private fragileContentPercentage;
+    private foodItemContentPercentage;
+    private liquidItemContentPercentage;
     
+    public Shipment(int fragPercent, int foodPercent, int liquidPercent)
+    {
+        this.fragileContentPercentage = fragPercent;
+        this.foodItemContentPercentage = foodPercent;
+        this.liquidItemContentPercentage = liquidPercent;
+    }
+
+
 }
