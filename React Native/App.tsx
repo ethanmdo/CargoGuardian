b76c3083a9150ec2b511eb7ad1d7336556e7f6ed
Ethan Do <ethando@gmail.com>
2023-09-15 21:42:32 -0400

    app.tsx changes

--- React Native/App.tsx
+++ React Native/App.tsx
@@ -1,20 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, TextInput, Button, Text, StyleSheet } from "react-native";
+import axios from "axios";
 
-export default function App() {
+const App: React.FC = () => {
+  const [shipmentContent, setShipmentContent] = useState<string>("");
+  const [route, setRoute] = useState<string>("");
+  const [weather, setWeather] = useState<string>("");
+  const [prediction, setPrediction] = useState<string>("");
+
+  const getRiskPrediction = async () => {
+    try {
+      const response = await axios.post<string>(
+        "http://localhost:8081/predict-risk",
+        {
+          shipmentContent,
+          route,
+          weatherConditions: weather,
+        }
+      );
+      setPrediction(response.data);
+    } catch (error) {
+      console.error("Error fetching prediction:", error);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.input}
+        placeholder="Shipment Content"
+        value={shipmentContent}
+        onChangeText={(text) => setShipmentContent(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Route"
+        value={route}
+        onChangeText={(text) => setRoute(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Weather Conditions"
+        value={weather}
+        onChangeText={(text) => setWeather(text)}
+      />
+
+      <Button title="Predict Risk" onPress={getRiskPrediction} />
+
+      {prediction && (
+        <Text style={styles.predictionText}>Risk: {prediction}</Text>
+      )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    padding: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+  predictionText: {
+    marginTop: 20,
+    fontSize: 18,
+    textAlign: "center",
   },
 });
+
+export default App;
