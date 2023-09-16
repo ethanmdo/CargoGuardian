82b767c2cfab570b7a50c114224099ee53299492
Aneesh Sunkarapalli <56797251+AneeshSunkarapalli@users.noreply.github.com>
2023-09-16 17:43:55 -0400

    overall edit

--- src/main/java/com/cargoguardian/model/SafetyScore.java
+++ src/main/java/com/cargoguardian/model/SafetyScore.java
@@ -13,23 +13,39 @@ public class SafetyScore{
         this.r = r;
     }
 
-    public int getShipmentScore()
+    public double getShipmentScore()
     {
         return this.s.getSafetyScore();
     }
 
-    public int getWeatherConditionsScore()
+    public double getWeatherConditionsScore()
     {
         return this.w.getSafetyScore();
     }
 
-    public int getRoadConditionsScore()
+    public double getRoadConditionsScore()
     {
         return this.r.getSafetyScore();
     }
 
-    public int overallSafetyScoreOutput()
+    public double overallSafetyScoreOutput()
     {
-        return 0;
+        double shipmentSafetyScore = this.getShipmentScore();
+        double weatherConditionScore = this.getWeatherConditionsScore();
+        double roadConditionScore = this.getRoadConditionsScore();
+
+        if (shipmentSafetyScore < 0 || shipmentSafetyScore > 100 ||
+            weatherConditionScore < 0 || weatherConditionScore > 100 ||
+            roadConditionScore < 0 || roadConditionScore > 100) {
+            throw new IllegalArgumentException("Scores must be between 0 and 100.");
+        }
+
+        double shipmentWeight = 0.5; 
+        double weatherWeight = 0.25;  
+        double roadWeight = 0.25;   
+
+        double overallSafetyScore = (shipmentSafetyScore * shipmentWeight) + (weatherConditionScore * weatherWeight) + (roadConditionScore * roadWeight);
+
+        return overallSafetyScore;
     }
 }
